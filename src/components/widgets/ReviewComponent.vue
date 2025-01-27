d3956ebe38ea0adb42af4fcebf990b7e32dafc51
Vladislav Sennikov <456un@mail.ru>
2025-01-27 17:38:29 +0300

    feat(отзывы): добавить блок с отзывами

---
+++ src/components/widgets/ReviewComponent.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  name: "ReviewComponent",
+  props: [
+    'name',
+    'link',
+  ],
+}
+</script>
+
+<template>
+  <div class="review-component">
+    <div class="review-avatar"></div>
+    <div class="review-content">
+      <a class="review-content-name" href="{{ link }}">{{ name }}</a>
+      <div class="review-text"><slot></slot></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.review-component {
+  display: flex;
+  padding: 0 60px;
+  margin-bottom: 60px;
+
+  .review-avatar {
+    background: transparent url("@/assets/img/avatar.png") no-repeat center center;
+    background-size: 54px 54px;
+    width: 54px;
+    height: 54px;
+  }
+
+  .review-content {
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+    width: calc(100% - 74px);
+
+    .review-content-name {
+      line-height: 1.1;
+      font-size: .875em;
+      color: white;
+      text-transform: uppercase;
+      text-decoration: none;
+      margin-top: 10px;
+      margin-bottom: 15px;
+    }
+
+    .review-text {
+      font-size: .875em;
+      letter-spacing: 1px;
+      text-align: justify;
+    }
+  }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,7 @@
 import BlockInfoComponent from '@/components/widgets/BlockInfoComponent.vue'
 import ProgressBarComponent from "@/components/ui/ProgressBarComponent.vue";
 import WorkExperienceComponent from "@/components/widgets/WorkExperienceComponent.vue";
+import ReviewComponent from "@/components/widgets/ReviewComponent.vue";
 
 export default {
   name: "HomePage.vue",
@@ -9,6 +10,7 @@ export default {
     BlockInfoComponent,
     ProgressBarComponent,
     WorkExperienceComponent,
+    ReviewComponent
   }
 }
 </script>
@@ -22,6 +24,7 @@ export default {
       решений. Основные технологии: Go, PHP, Postgres, Laravel, Yii2, Symfony, Vue и др.
     </div>
   </BlockInfoComponent>
+
   <BlockInfoComponent id="#skills">
     <h2 class="title">Скиллы</h2>
     <div class="columns">
@@ -39,6 +42,7 @@ export default {
         <ProgressBarComponent value="40" title="MongoDB"/>
       </div>
     </div>
+
     <div class="columns">
       <div class="col">
         <div class="col-title">Фреймворки/CMS</div>
@@ -55,26 +59,58 @@ export default {
       </div>
     </div>
   </BlockInfoComponent>
+
   <BlockInfoComponent id="works">
     <h2 class="title">Опыт работы</h2>
     <WorkExperienceComponent startYear="2011" endYear="2013" title="Лаборант" place="КГУ им. Н.А. Некрасова">
       Администрирование компьютерных классов, ремонт компьютеров, установка ОС, настройка и установка ПО
     </WorkExperienceComponent>
+
     <WorkExperienceComponent startYear="2013" endYear="2014" title="Технолог" place="КГУ им. Н.А. Некрасова">
       Администрирование компьютерных классов, ремонт компьютеров, установка ОС, настройка и установка ПО,
       работа с дистанционной системой обучения Moodle, разработка ПО на языке программирования C#
     </WorkExperienceComponent>
+
     <WorkExperienceComponent startYear="2014" endYear="2015" title="Web-программист" place="Веб-студия WSDV">
       Верстка и программирование сайтов, работа с 1C-Битрикс, создание сайтов визиток
     </WorkExperienceComponent>
-    <WorkExperienceComponent startYear="2016" endYear="2017" title="Fullstack разработчик 1С-Битрикс" place="Веб-студия Digitalwand">
+
+    <WorkExperienceComponent startYear="2016" endYear="2017" title="Fullstack разработчик 1С-Битрикс"
+                             place="Веб-студия Digitalwand">
       Разработка функционала различной сложности, для сайтов работающих на 1C-Битрикс и Битрикс24
     </WorkExperienceComponent>
+
     <WorkExperienceComponent startYear="2017" endYear="2024" title="Ведущий бэкенд разработчик" place="SOKOLOV">
       Разработка центра единой авторизации (SSO), поддержка и доработка geo сервиса,
       поддержка и доработка корпоративного портала на Битрикс24, развитие сайта sokolov.ru,
       разработка сервиса онлайн оплат
     </WorkExperienceComponent>
+  </BlockInfoComponent>
+
+  <BlockInfoComponent id="edicatons">
+    <h2 class="title">Образование</h2>
+    <WorkExperienceComponent startYear="2010" endYear="2015" title="Учитель информатики и математики"
+                             place="КГУ им. Н.А. Некрасова">
+    </WorkExperienceComponent>
+  </BlockInfoComponent>
+
+  <BlockInfoComponent id="reviews">
+    <h2 class="title">Отзывы</h2>
+    <ReviewComponent name="Анна" link="https://freelance.ru/krinic">
+      Как всегда четко и профессионально!
+    </ReviewComponent>
+
+    <ReviewComponent name="Анна" link="https://freelance.ru/krinic">
+      Очень качественно выполненная работа! Исполнитель - профессионал и прекрасно разбирается в Битрикс!
+    </ReviewComponent>
+
+    <ReviewComponent name="Илья Кузнецов" link="https://freelance.ru/ilay-1">
+      Сделал все быстро и профессионально! Отлично!
+    </ReviewComponent>
+
+    <ReviewComponent name="iqcon" link="https://freelance.ru/iqcon">
+      Работу выполнил хорошо, советую для сотрудничества!
+    </ReviewComponent>
   </BlockInfoComponent>
 </template>
 
